87bb91d6a1816f9a8b4e1686d1bb886362d0d624
gsnie <gsniegow@gmail.com>
2021-06-11 11:25:09 -0500

    Initial commit

---
+++ src/main/java/guru/springframework/spring6jokesappv2/Spring6JokesAppV2Application.java
@@ -0,0 +1,13 @@
+package guru.springframework.spring6jokesappv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6JokesAppV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring6JokesAppV2Application.class, args);
+    }
+
+}
---
+++ src/test/java/guru/springframework/spring6jokesappv2/Spring6JokesAppV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.spring6jokesappv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6JokesAppV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
