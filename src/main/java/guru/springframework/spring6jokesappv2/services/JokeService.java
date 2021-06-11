d720c860f23b5ebb5a64d9fc0493ce84b48e8e01
gsnie <gsniegow@gmail.com>
2021-06-11 11:38:43 -0500

    Implementing Service

---
+++ src/main/java/guru/springframework/spring6jokesappv2/services/JokeService.java
@@ -0,0 +1,6 @@
+package guru.springframework.spring6jokesappv2.services;
+
+public interface JokeService {
+
+    public String getJoke();
+}
---
+++ src/main/java/guru/springframework/spring6jokesappv2/services/JokeServiceImpl.java
@@ -0,0 +1,13 @@
+package guru.springframework.spring6jokesappv2.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JokeServiceImpl implements  JokeService
+{
+
+    @Override
+    public String getJoke() {
+        return null;
+    }
+}
